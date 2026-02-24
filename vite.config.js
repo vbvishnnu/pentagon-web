b2192a3e6cdfa71a19e9e8b2106165cf2c2a0d5d
Vishnu <vishnu@Vishnnus-MacBook-Pro.local>
2026-02-24 15:50:28 +0530

    Add docs build for GitHub Pages deployment from main branch

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+    base: '/pentagon-web/',
+    plugins: [react()],
+    build: {
+        outDir: 'docs'
+    },
+    server: {
+        port: 5173,
+        open: true
+    }
 })
