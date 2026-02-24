2cbac280ecd13f3ed9456e6e9a4e4d327b471a20
Vishnu <vishnu@Vishnnus-MacBook-Pro.local>
2026-02-24 16:11:13 +0530

    Fix: Remove base path for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/pentagon-web/',
     plugins: [react()],
     build: {
         outDir: 'docs'
